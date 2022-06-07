3ed8fbfd92224804bc06e92dbf6badc7a2acd32a
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-07 23:09:12 +0200

    Update P7_05_streamlit_app_final.py

--- P7_05_streamlit_app_final.py
+++ P7_05_streamlit_app_final.py
@@ -127,5 +127,18 @@ l3.subheader("EXT_SOURCE_3 x Education Type")
 l3.plotly_chart(fig12, use_container_width=True)
 
 
+#ajout informations concernant variables:
+st.subheader("Définition des groupes")
+    st.markdown("\
+    \n\
+    * **Client** : la valeur pour le client considéré\n\
+    * Moyenne : valeur moyenne pour l'ensemble des clients\n\
+    * En Règle : valeur moyenne pour l'ensemble des clients en règle\n\
+    * En Défaut : valeur moyenne pour l'ensemble des clients en défaut\n\
+    * Similaires : valeur moyenne pour les 20 clients les plus proches du client\
+    considéré sur les critères sexe/âge/revenu/durée/montant du crédit\n\n\
+    ")
+
+
 #Run sur streamlit: streamlit run "C:/Users/kathl/Desktop/Projet7_OP/streamlit_app.py
 
